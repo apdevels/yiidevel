ed953114b0ba4a5273d8c6c89e37e5e657221325
Andrey Plotnikov <66311522+apdevels@users.noreply.github.com>
2020-08-14 22:20:40 +0300

    Update db.php

--- backend/config/db.php
+++ backend/config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yiidevel',
-    'username' => 'ppp',
-    'password' => 'gate3og',
+    'username' => 'xxx',
+    'password' => 'xxx',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
